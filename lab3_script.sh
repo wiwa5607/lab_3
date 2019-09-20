4cf7fded6e9f1bb3b1c13b45f45a8424b85cfdbd
wiwa5607 <wiwa5607@colorado.edu>
2019-09-20 16:31:01 -0600

    Finished lab3_script.sh

--- lab3_script.sh
+++ lab3_script.sh
@@ -2,10 +2,32 @@
 # Author: Will Walker
 # Date: 9/19/2019
 
-#Problem 1 Code
+# Problem 1 Code
 
 echo "Input file name: "
 read fileName
 echo "Enter expression: "
 read expr
-grep $fileName -c -e "$expr"
+echo "Enter options: "
+read ops
+grep $ops "$expr" $fileName
+
+# phone numbers
+# [0-9][10]
+# -E -c
+grep -E -c [0-9][10] regex_practice.txt
+# count emails
+# @
+# -c
+grep -c @ regex_practice.txt
+# phone numbers in 303
+# 303-
+# -o
+grep -o 303- regex_practice.txt
+
+# store emails
+# geocities.com
+# >> email_results.txt
+grep geocities.com regex_practice.txt >>email_results.txt
+
+
